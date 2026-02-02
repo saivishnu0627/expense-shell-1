9fc8b7a8ade1c8c6f9da3f887ca97dee0398b419
saivishnu0627 <saivishnu0627@gmail.com>
2026-02-02 00:06:32 -0600

    functions

--- mysql.sh
+++ mysql.sh
@@ -7,14 +7,14 @@ check_root
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Server"
+dnf install mysql-serveeer -y &>>$LOGFILE
+#VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>> $LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+#VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Sever"
+#VALIDATE $? "Starting MySQL Sever"
 
 mysql -h 172.31.27.211 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
