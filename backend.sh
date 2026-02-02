a1fb42725a85a100810e04fb0103c21d4db81fdb
saivishnu0627 <saivishnu0627@gmail.com>
2026-02-02 00:25:05 -0600

    functions

--- backend.sh
+++ backend.sh
@@ -34,7 +34,7 @@ unzip /tmp/backend.zip &>>$LOGFILE
 npm install &>>$LOGFILE
 
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 
 systemctl daemon-reload &>>$LOGFILE
